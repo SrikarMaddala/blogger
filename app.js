3315cd7b7eff9b2f431935e7a51ee9d040604b17
Srikar Maddala <maddalasrikar19@gmail.com>
2023-06-24 14:43:08 +0530

    update all ejs , basic working

--- app.js
+++ app.js
@@ -15,17 +15,37 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts =[];   // let instead var for globals
+
+app.get("/" , function(req,res){
+  res.render("Home" , {
+    homecontent : homeStartingContent ,
+  posts :posts
+  });
+});
+ 
+app.get("/about" , function(req,res){
+  res.render("about" , {abcon : aboutContent});
+});
+
+app.get("/contact", function(req,res){
+  res.render("contact" , {contactinfo : contactContent});
+});
+
+app.get("/compose" , function(req,res){
+  res.render("compose");
+});
 
 
 
-
-
-
-
-
-
-
-
+app.post("/compose" , function(req,res){
+  const post =  {
+    title : req.body.postTitle,
+    content : req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+});
 
 
 app.listen(3000, function() {
